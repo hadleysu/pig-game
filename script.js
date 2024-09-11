3990f69e55cd87e182fcde6a1762a6d123daca80
hadley <hadley.susu@gmail.com>
2024-09-11 11:17:39 +0800

    feat: initialize game setup

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const btnRollEl = document.querySelector('.btn--new');
+const btnHoldEl = document.querySelector('.btn--hold');
+const btnNewEl = document.querySelector('.btn--new');
+const score0El = document.getElementById('score--0');
+const score1El = document.getElementById('score--1');
+const diceEl = document.querySelector('.dice');
+
+let score;
+
+// Initialize the game
+const init = function () {
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  diceEl.classList.add('hidden');
+};
+init();
